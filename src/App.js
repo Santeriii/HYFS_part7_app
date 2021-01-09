1c9f531540d2220870eefb97f5c8c7a4e9002d71
Santeri Einola <62256499+Santeriii@users.noreply.github.com>
2021-01-09 12:27:15 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import {
+    BrowserRouter as Router,
+    Switch, Route, Link
+} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Home = () => (
+    <div> <h2>TKTL notes app</h2> </div>
+)
+
+const Notes = () => (
+    <div> <h2>Notes</h2> </div>
+)
+
+const Users = () => (
+    <div> <h2>Users</h2> </div>
+)
+
+const App = () => {
+
+    const padding = {
+        padding: 5
+    }
+
+    return (
+        <Router>
+            <div>
+                <Link style={padding} to="/">home</Link>
+                <Link style={padding} to="/notes">notes</Link>
+                <Link style={padding} to="/users">users</Link>
+            </div>
+
+            <Switch>
+                <Route path="/notes">
+                    <Notes />
+                </Route>
+                <Route path="/users">
+                    <Users />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+
+            <div>
+                <i>Note app, Department of Computer Science 2021</i>
+            </div>
+        </Router>
+    )
 }
 
-export default App;
+export default App
