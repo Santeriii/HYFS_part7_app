21b3a0dc1facd6e6fcefef8a23b08e19b689677c
Santeri Einola <62256499+Santeriii@users.noreply.github.com>
2021-01-12 10:44:40 +0200

    bootsrap-design added

--- src/App.js
+++ src/App.js
@@ -1,52 +1,190 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import { Button, Form, Table, Alert, Navbar, Nav } from 'react-bootstrap'
+
 import {
-    BrowserRouter as Router,
-    Switch, Route, Link
-} from 'react-router-dom'
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useParams,
+  useHistory,
+} from "react-router-dom"
 
 const Home = () => (
-    <div> <h2>TKTL notes app</h2> </div>
+  <div> 
+    <h2>TKTL notes app</h2> 
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p> 
+  </div>
 )
 
-const Notes = () => (
-    <div> <h2>Notes</h2> </div>
+const Note = ({ notes }) => {
+  const id = useParams().id
+  const note = notes.find(n => n.id === Number(id))
+  return (
+    <div>
+      <h2>{note.content}</h2>
+      <div>{note.user}</div>
+      <div><strong>{note.important ? 'tärkeä' : ''}</strong></div>
+    </div>
+  )
+}
+
+const Notes = (props) => (
+  <div>
+    <h2>Notes</h2>
+    <Table striped>
+        <tbody>
+        {props.notes.map(note =>
+            <tr key={note.id}>
+                <td>
+                    <Link to={`/notes/${note.id}`}>{note.content}</Link>
+                </td>
+            </tr>
+        )}
+        </tbody>
+    </Table>
+  </div>
 )
 
 const Users = () => (
-    <div> <h2>Users</h2> </div>
+  <div>
+    <h2>TKTL notes app</h2>
+    <ul>
+      <li>Matti Luukkainen</li>
+      <li>Juha Tauriainen</li>
+      <li>Arto Hellas</li>
+    </ul>
+  </div>
 )
 
+const Login = (props) => {
+  const history = useHistory()
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    props.onLogin('mluukkai')
+    history.push('/')
+  }
+
+  return (
+    <div>
+      <h2>login</h2>
+      <form onSubmit={onSubmit}>
+        <Form.Group>
+            <Form.Label>username:</Form.Label>
+            <Form.Control
+                type="text"
+                name="username"
+            />
+            <Form.Label>password:</Form.Label>
+            <Form.Control
+                type="pasword"
+            />
+            <Button variant="primary" type="submit">
+                login
+            </Button>
+        </Form.Group>
+      </form>
+    </div>
+  )
+}
+
 const App = () => {
-
-    const padding = {
-        padding: 5
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      content: 'HTML is easy',
+      important: true,
+      user: 'Matti Luukkainen'
+    },
+    {
+      id: 2,
+      content: 'Browser can execute only Javascript',
+      important: false,
+      user: 'Matti Luukkainen'
+    },
+    {
+      id: 3,
+      content: 'Most important methods of HTTP-protocol are GET and POST',
+      important: true,
+      user: 'Arto Hellas'
     }
+  ])
 
-    return (
-        <Router>
-            <div>
+  const [user, setUser] = useState(null)
+  const [message, setMessage] = useState(null)
+
+  const login = (user) => {
+    setUser(user)
+    setMessage(`welcome ${user}`)
+    setTimeout(() => {
+        setMessage(null)
+    }, 10000)
+  }
+
+  const padding = {
+    padding: 5
+  }
+
+  return (
+    <div class="container">
+    <Router>
+        <div class="container">
+            {(message &&
+                <Alert variant="success">
+                    {message}
+                </Alert>
+            )}
+        </div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+            <Nav.Link href="#" as="span">
                 <Link style={padding} to="/">home</Link>
+            </Nav.Link>
+            <Nav.Link href="#" as="span">
                 <Link style={padding} to="/notes">notes</Link>
+            </Nav.Link>
+            <Nav.Link href="#" as="span">
                 <Link style={padding} to="/users">users</Link>
-            </div>
+            </Nav.Link>
+            <Nav.Link href="#" as="span">
+                {user
+                ? <em>{user} logged in</em>
+                : <Link to="/login">login</Link>
+                }
+            </Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+        </Navbar>
 
-            <Switch>
-                <Route path="/notes">
-                    <Notes />
-                </Route>
-                <Route path="/users">
-                    <Users />
-                </Route>
-                <Route path="/">
-                    <Home />
-                </Route>
-            </Switch>
-
-            <div>
-                <i>Note app, Department of Computer Science 2021</i>
-            </div>
-        </Router>
-    )
+      <Switch>
+        <Route path="/notes/:id">
+          <Note notes={notes} />
+        </Route>
+        <Route path="/notes">
+          <Notes notes={notes} />
+        </Route>
+        <Route path="/users">
+          {user ? <Users /> : <Redirect to="/login" />}
+        </Route>
+        <Route path="/login">
+          <Login onLogin={login} />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>      
+      <div>
+        <br />
+        <em>Note app, Department of Computer Science 2020</em>
+      </div>
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Counter from './Counter';
+import App from './App'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counter />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
